b36c98dcf29cdc9791972d6a69b75c763983ab9b
asanov2610 <139971292+asanov2610@users.noreply.github.com>
2023-07-25 13:33:28 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -2,5 +2,5 @@ def get_upper_text(text):
     return str.upper(text)
 
 def get_tittle_text(text):
-    """Возвращает текст с заглавными буквами каждого слова"""
+    """Возвращает текст с заглавными буквами каждого слова в строке"""
     return str.title(text)
