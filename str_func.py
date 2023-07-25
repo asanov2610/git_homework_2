4c83f18cf4dae905a3d3f5040649bca5520d57e9
Arlen Asanov <a.asanov2610@gmail.com>
2023-07-25 13:43:10 +0300

    add func get_tittle

--- str_func.py
+++ str_func.py
@@ -1,6 +1,9 @@
 def get_upper_text(text):
+    """Добавил описание функции"""
+    # Исправили баги
     return str.upper(text)
 
 def get_tittle_text(text):
     """Возвращает текст с заглавными буквами каждого слова"""
     return str.title(text)
+
